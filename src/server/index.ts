280ca4745336365cc787d5a51c1d8b89299b72f6
atrociousdinner <nerishcodes@gmail.com>
2026-06-19 12:59:07 +0545

    basic setup

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,23 @@
+import amqp from "amqplib"
+
+
 async function main() {
+
+  const connection = 'amqp://guest:guest@localhost:5672/'
+  const conn = await amqp.connect(connection)
+
+
   console.log("Starting Peril server...");
+
+
+  process.on("SIGINT", async () => {
+    console.log("Shutting down")
+
+    await conn.close();
+
+    process.exit(0);
+
+  })
 }
 
 main().catch((err) => {
